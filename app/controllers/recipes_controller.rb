a86320f54179dd70d0bc421878b4ef75f6cad057
hirofumitorato <hirofumitorato@gmail.com>
2020-05-18 13:42:10 +0900

    valid

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,9 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+
   def index
+    @recipes = Recipe.all
   end
 
   def show
@@ -13,14 +17,34 @@ class RecipesController < ApplicationController
   def create
     @recipe = current_user.recipes.build(recipe_params)
     if @recipe.save
-      redirect_to recipe_path(@recipe)
+      redirect_to recipe_path(@recipe), notice: "レシピを投稿しました。"
     else
       render :new
     end
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
+    if @recipe.user != current_user
+      redirect_to recipes_path, alert: "不正なアクセスです。"  
+   end
   end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe), notice: "レシピを更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to user_path(recipe.user), notice: "レシピを削除しました。"
+  end
+
 
   private
   def recipe_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+
   def index
     @users = User.all
   end
@@ -9,13 +12,19 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user), alert: "不正なアクセスです。"
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
-  end
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "ユーザー情報を更新しました。"
+    else
+      render :edit
+    end
+ end
 
 
   private
