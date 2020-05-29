62f6bd126a288efd359a7ce041c6b5d40416291f
hirofumitorato <hirofumitorato@gmail.com>
2020-05-29 11:09:22 +0900

    iine

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :profile_image
   has_many :recipes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def already_favorited?(recipe)
+    self.favorites.exists?(recipe_id: recipe.id)
+  end
 
   validates :username, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users
-  resources :recipes
+  resources :recipes do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
+
